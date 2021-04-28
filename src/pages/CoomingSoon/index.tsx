cb4f9db7876d39d9a9ad2d9096d8143082544474
sirzes02 <sirzes02@gmail.com>
2021-04-28 15:43:01 -0500

    tabBar and comming soon

---
+++ src/pages/CoomingSoon/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper, Heading } from "./styles";
+
+const Following: React.FC = () => {
+  return (
+    <Wrapper>
+      <Heading>Comming Soon...</Heading>
+    </Wrapper>
+  );
+};
+
+export default Following;
---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+const CoomingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Text>Comming Soon</Text>
+    </Container>
+  );
+};
+
+export default CoomingSoon;
